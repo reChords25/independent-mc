b61afe0af52ff740a6e1da484ac4187b643512e2
rechords25 <rechords25.apps@gmail.com>
2024-09-18 21:03:02 +0200

    Added dependency download Changed console info formatting Added explanation to mods.txt

--- download.bat
+++ download.bat
@@ -9,41 +9,41 @@ if ERRORLEVEL 1 GOTO NOPYTHON
 goto :HASPYTHON
 
 :NOPYTHON
-echo [ERROR] Python is not installed or on PATH.
-echo [INFO] Install Python or consider adding the Path of python.exe to PATH.
+echo [ERROR] Python is not installed or on PATH
+echo [INFO] Install Python or consider adding the Path of python.exe to PATH
 echo [INFO] Press any key to exit...
 pause >NUL
 goto :eof
 
 :HASPYTHON
-echo [INFO] Python installation found.
-echo [INFO] Creating python virtual environment.
+echo [INFO] Python installation found
+echo [INFO] Creating python virtual environment
 python -m venv "%cd%\.venv"
 set "venvpy=%cd%\.venv\Scripts\python.exe"
 
-echo [INFO] Installing portablemc via pip.
+echo [INFO] Installing portablemc via pip
 "%venvpy%" -m pip install --no-input portablemc[certifi] >NUL 2>NUL
 
-echo [INFO] Installing Minecraft %loader_type%, version %mc_version% into "%wd%".
+echo [INFO] Installing Minecraft %loader_type%, version %mc_version% into "%wd%"
 echo[
 "%venvpy%" -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human start %loader_type%:%mc_version% --dry
 echo[
 
-echo [INFO] Installing mods.
+echo [INFO] Installing mods
 echo[
 "%venvpy%" -m pip install --no-input requests >NUL 2>NUL
 "%venvpy%" install_mods.py %loader_type% %mc_version%
 "%venvpy%" -m pip uninstall -y requests >NUL 2>NUL
 echo[
 
-echo [INFO] Creating start.bat file.
+echo [INFO] Creating start.bat file
 (
    echo set "wd=%cd%\minecraft\wd"
    echo set "md=%cd%\minecraft\md"
    echo start .venv\Scripts\python.exe -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human-color start %loader_type%:%mc_version%
 ) > start.bat
 
-echo [INFO] Everything done.
+echo [INFO] Everything done
 echo [INFO] Press any key to exit...
 pause >NUL
 goto :eof
--- install_mods.py
+++ install_mods.py
@@ -1,70 +1,104 @@
 import os, sys, requests
 
-MODS_FILE = "mods.txt"  # The file containing the URLs to the modrinth mods
+MODS_FILE = "mods.txt"
 MODS_FOLDER = os.getcwd() + "\\minecraft\\wd\\mods"
 LOADER_TYPE = sys.argv[1]
 MINECRAFT_VERSION = sys.argv[2]
 
 API_URL = "https://api.modrinth.com/v2"
+visited_mods = set()
 
-def get_mod_info(url):
-    # Mod URL structure: https://modrinth.com/mod/{mod_id}
-    response = requests.get(f'{API_URL}/project/{url.rstrip('/').split('/')[-1]}')
-
+def get_mod_info(url: str) -> tuple[str, str]:
+    response = requests.get(f'{API_URL}/project/{url.rstrip("/").split("/")[-1]}')
     if response.status_code != 200: return None
-    return response.json()['title'], response.json()['id']
+    mod_info = response.json()
+    return mod_info['title'], mod_info['id']
 
-def get_latest(mod_id):
+def get_latest(mod_id: str) -> str:
     version_url = f'{API_URL}/project/{mod_id}/version'
     response = requests.get(version_url)
-
+    
     if response.status_code != 200: return None
     versions = response.json()
     for version in versions:
         if MINECRAFT_VERSION in version['game_versions'] and LOADER_TYPE in version['loaders'] and version['version_type'] == 'release':
             return version
+    return None
 
-def download_mod(version_info):
+def download_mod(version_info: str) -> str:
     for file in version_info['files']:
         if file['primary']:
             download_url = file['url']
             file_name = file['filename']
             mod_path = os.path.join(MODS_FOLDER, file_name)
             
-            # Download and save the file
             with requests.get(download_url, stream=True) as r:
                 r.raise_for_status()
                 with open(mod_path, 'wb') as f:
                     for chunk in r.iter_content(chunk_size=8192):
                         f.write(chunk)
-            break
-    return file_name
+            return file_name
+    return None
 
-def download_mods():
+def get_mod_title(mod_id: str) -> str:
+    response = requests.get(f'{API_URL}/project/{mod_id}')
+    if response.status_code != 200: return None
+    return response.json()['title']
+
+def get_dependencies(mod_version: str, parent_mod_title: str) -> None:
+    if not 'dependencies' in mod_version: return
+    count = len(mod_version['dependencies'])
+    if count == 0: return
+    
+    print(f'[INFO] Found {count} {'dependency' if count == 1 else 'dependencies'} for {parent_mod_title}')
+    i = 1
+    for dependency in mod_version['dependencies']:
+        if not (dependency['dependency_type'] == 'required' and dependency['project_id']): return
+        mod_id = dependency['project_id']
+
+        if mod_id in visited_mods: continue
+        
+        mod_title = get_mod_title(mod_id)
+        print(f'[INFO] Dependency {i}: {mod_title}')
+        
+        mod_version = get_latest(mod_id)
+        if not mod_version:
+            print(f'[ERROR] No compatible version found for dependent mod {mod_title}')
+            print(f'[INFO] You might have to delete the file for {parent_mod_title} yourself.')
+            return
+        print(f'[INFO] Downloading dependent mod {mod_title}')
+        file_name = download_mod(mod_version)
+        print(f'[INFO] Successfully downloaded as "{file_name}"')
+        visited_mods.add(mod_id)
+        get_dependencies(mod_version, mod_title)
+        i += 1
+
+
+if __name__ == "__main__":
+    if not os.path.exists(MODS_FOLDER):
+        os.makedirs(MODS_FOLDER)
     if not os.path.exists(MODS_FILE):
         print(f'[ERROR] Mods file "{MODS_FILE}" not found.')
         print(f'[INFO] Create the file and paste the mod URLs or move it to the right directory.')
-        return
+        exit()
     
     with open(MODS_FILE, 'r') as f:
         mod_urls = f.readlines()
     
     for url in mod_urls:
         url = url.strip()
-        if not url or url[0] == '#':
-            continue
+        if not url or url[0] == '#': continue
         
-        mod_name, mod_id = get_mod_info(url)
-        print(f'[INFO] Fetching mod: {mod_name}')
-        
+        mod_title, mod_id = get_mod_info(url)
+        if mod_id in visited_mods: continue
+        print(f'[INFO] Fetching mod info for {mod_title}')
         mod_version = get_latest(mod_id)
-        if mod_version:
-            file_name = download_mod(mod_version)
-            print(f'[INFO] Downloaded {mod_name} as "{file_name}"')
-        else:
-            print(f'[ERROR] No compatible version found for mod {mod_id}')
-
-if __name__ == "__main__":
-    if not os.path.exists(MODS_FOLDER):
-        os.makedirs(MODS_FOLDER)
-    download_mods()
+        if not mod_version:
+            print(f'[ERROR] No compatible version found for mod {mod_title}')
+            continue
+        print(f'[INFO] Downloading {mod_title}')
+        file_name = download_mod(mod_version)
+        print(f'[INFO] Successfully downloaded as "{file_name}"')
+        visited_mods.add(mod_id)
+        
+        get_dependencies(mod_version, mod_title)
