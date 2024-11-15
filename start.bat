b1e7f052b43bf63261560769482dd60ef0429b71
reChords25 <125921380+reChords25@users.noreply.github.com>
2024-11-15 13:07:46 +0100

    Delete start.bat

--- start.bat
+++
@@ -1,3 +0,0 @@
-set "wd=D:\PC\Windows Folders\Dokumente\coding und tech projekte\independent-mc\minecraft\wd"
-set "md=D:\PC\Windows Folders\Dokumente\coding und tech projekte\independent-mc\minecraft\md"
-start .venv\Scripts\python.exe -m portablemc --main-dir "D:\PC\Windows Folders\Dokumente\coding und tech projekte\independent-mc\minecraft\md" --work-dir "D:\PC\Windows Folders\Dokumente\coding und tech projekte\independent-mc\minecraft\wd" --output human-color start fabric:1.21
