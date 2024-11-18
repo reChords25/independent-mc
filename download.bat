948c03e35cf146b0390c3d5ea7a4bdc2db200df3
rechords25 <rechords25.apps@gmail.com>
2024-11-18 21:49:10 +0100

    Added authentication for multiplayer Fixed loaders in start.bat

--- download.bat
+++ download.bat
@@ -1,7 +1,13 @@
 @echo off
+:: Set loader (vanilla, fabric, forge, neoforge, legacyfabric, quilt)
 set "loader_type=fabric"
+:: Set Minecraft Version
 set "mc_version=1.21"
-set "install_mods=true"
+:: Whether to install mods, resourcepacks etc.
+set "install_content=false"
+:: Email address to use for authentication (Microsoft account)
+:: Only really  necessary for multiplayer! Leave emtpy if not needed
+set "auth_mail="
 
 set "wd=%cd%\minecraft\wd"
 set "md=%cd%\minecraft\md"
@@ -31,23 +37,31 @@ echo[
 "%venvpy%" -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human start %loader_type%:%mc_version% --dry
 echo[
 
-if "%install_mods%%" == "true" (
+if "%install_content%" == "true" (
     echo [INFO] Installing additional content
     echo[
     "%venvpy%" -m pip install --no-input requests >NUL 2>NUL
     "%venvpy%" additional-content-downloader\main.py %loader_type% %mc_version%
     "%venvpy%" -m pip uninstall -y requests >NUL 2>NUL
     echo[
-) else if "%install_mods%%" == "false" (
+) else if "%install_content%" == "false" (
     echo [INFO] Skipping additional content installation
     echo[
 )
 
+if "%loader_type%" == "vanilla" (
+    set "loader_type="
+) else (
+    set "loader_type=%loader_type%:"
+)
+
 echo [INFO] Creating start.bat file
 (
-   echo set "wd=%cd%\minecraft\wd"
-   echo set "md=%cd%\minecraft\md"
-   echo start .venv\Scripts\python.exe -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human-color start %loader_type%:%mc_version%
+   if "%auth_mail%" == "" (
+       echo start .venv\Scripts\python.exe -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human-color start %loader_type%%mc_version%
+   ) else (
+       echo start .venv\Scripts\python.exe -m portablemc --main-dir "%md%" --work-dir "%wd%" --output human-color start --login %auth_mail% %loader_type%%mc_version%
+   )
 ) > start.bat
 
 echo [INFO] Everything done
